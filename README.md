579ad3f276c53b5ef812633f438c4208965dbbd4
Xing Li <msuli_1120@ymail.com>
2017-06-29 20:28:53 -0700

    todo list create

---
+++ README.md
@@ -0,0 +1,4 @@
+symfony_first
+=============
+
+A Symfony project created on June 29, 2017, 6:43 pm.
---
+++ src/AppBundle/Controller/TodoController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\Todo;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
+
+class TodoController extends Controller
+{
+  /**
+   * @Route("/", name="todo_list")
+   */
+  public function listAction(Request $request)
+  {   
+    $todos = $this->getDoctrine()
+            ->getRepository('AppBundle:Todo')
+            ->findAll();
+    return $this->render('todo/index.html.twig', array(
+      'todos' => $todos
+    ));
+  }
+
+    /**
+   * @Route("/todo/create", name="todo_create")
+   */
+  public function createAction(Request $request)
+  {   
+    $todo = new Todo;
+    $form = $this->createFormBuilder($todo)
+          ->add('name', TextType::class, array('attr' => array('class' => 'form-control', 'style'=>'margin-bottom:15px')))
+          ->add('category', TextType::class, array('attr' => array('class' => 'form-control', 'style'=>'margin-bottom:15px')))
+          ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control', 'style'=>'margin-bottom:15px')))
+          ->add('priority', ChoiceType::class, array('choices' => array('Low' => 'Low', 'Normal' => 'Normal', 'High' => 'High'),'attr' => array('class' => 'form-control', 'style'=>'margin-bottom:15px')))
+          ->add('due_date', DateTimeType::class, array('attr' => array('style'=>'margin-bottom:15px')))
+          ->add('save', SubmitType::class, array('label' => 'Create', 'attr' => array('class' => 'btn btn-info')))
+          ->getForm();
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $name = $form['name']->getData();
+      $category = $form['category']->getData();
+      $description = $form['description']->getData();
+      $priority = $form['priority']->getData();
+      $due_date = $form['due_date']->getData();
+      $now = new\DateTime('now');
+      $todo->setName($name);
+      $todo->setCategory($category);
+      $todo->setDescription($description);
+      $todo->setPriority($priority);
+      $todo->setDueDate($due_date);
+      $todo->setCreateDate($now);
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($todo);
+      $em->flush();
+      $this->addFlash('notice', 'Created successfully!'); 
+      return $this->redirectToRoute('todo_list'); 
+    } else {
+      return $this->render('todo/create.html.twig', array(
+        'form' => $form->createView()
+      ));
+    }
+    
+  }
+
+    /**
+   * @Route("/todo/edit/{id}", name="todo_edit")
+   */
+  public function editAction($id, Request $request)
+  {   
+    return $this->render('todo/edit.html.twig');
+  }
+
+    /**
+   * @Route("/todo/details/{id}", name="todo_details")
+   */
+  public function detailsAction($id)
+  {   
+    // replace this example code with whatever you need
+    return $this->render('todo/details.html.twig');
+  }
+}
