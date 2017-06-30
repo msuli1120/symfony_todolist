61b0ce60852d4f14c333af27226849b4f335144b
Xing Li <msuli_1120@ymail.com>
2017-06-29 20:46:10 -0700

    add edit function

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -74,7 +74,50 @@ class TodoController extends Controller
    */
   public function editAction($id, Request $request)
   {   
-    return $this->render('todo/edit.html.twig');
+    $todo = $this->getDoctrine()
+            ->getRepository('AppBundle:Todo')
+            ->find($id);
+    $todo->setName($todo->getName());
+    $todo->setCategory($todo->getCategory());
+    $todo->setDescription($todo->getDescription());
+    $todo->setPriority($todo->getPriority());
+    $todo->setDueDate($todo->getDueDate());
+    $todo->setCreateDate($todo->getCreateDate());
+    $form = $this->createFormBuilder($todo)
+          ->add('name', TextType::class, array('attr' => array('class' => 'form-control', 'style'=>'margin-bottom:15px')))
+          ->add('category', TextType::class, array('attr' => array('class' => 'form-control', 'style'=>'margin-bottom:15px')))
+          ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control', 'style'=>'margin-bottom:15px')))
+          ->add('priority', ChoiceType::class, array('choices' => array('Low' => 'Low', 'Normal' => 'Normal', 'High' => 'High'),'attr' => array('class' => 'form-control', 'style'=>'margin-bottom:15px')))
+          ->add('due_date', DateTimeType::class, array('attr' => array('style'=>'margin-bottom:15px')))
+          ->add('save', SubmitType::class, array('label' => 'Update', 'attr' => array('class' => 'btn btn-info')))
+          ->getForm();
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $name = $form['name']->getData();
+      $category = $form['category']->getData();
+      $description = $form['description']->getData();
+      $priority = $form['priority']->getData();
+      $due_date = $form['due_date']->getData();
+      $now = new\DateTime('now');
+      $em = $this->getDoctrine()->getManager();
+      $todo = $em->getRepository('AppBundle:Todo')->find($id);
+      $todo->setName($name);
+      $todo->setCategory($category);
+      $todo->setDescription($description);
+      $todo->setPriority($priority);
+      $todo->setDueDate($due_date);
+      $todo->setCreateDate($now);
+
+      $em->persist($todo);
+      $em->flush();
+      $this->addFlash('notice', 'Updated successfully!'); 
+      return $this->redirectToRoute('todo_list'); 
+    }
+
+    return $this->render('todo/edit.html.twig', array(
+      'todo' => $todo,
+      'form' => $form->createView(),
+    ));
   }
 
     /**
@@ -82,7 +125,11 @@ class TodoController extends Controller
    */
   public function detailsAction($id)
   {   
-    // replace this example code with whatever you need
-    return $this->render('todo/details.html.twig');
+    $todo = $this->getDoctrine()
+            ->getRepository('AppBundle:Todo')
+            ->find($id);
+    return $this->render('todo/details.html.twig', array(
+      'todo' => $todo
+    ));
   }
 }
